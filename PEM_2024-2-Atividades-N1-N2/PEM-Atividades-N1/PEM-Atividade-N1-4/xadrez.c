eb5520444dacf0a2e73160e484b43d181d7652f5
Alicia Abade <144282278+Lilimunz@users.noreply.github.com>
2024-08-31 20:38:22 -0300

    Corrigindo nome das peças xadrez.c

--- PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-4/xadrez.c
+++ PEM_2024-2-Atividades-N1-N2/PEM-Atividades-N1/PEM-Atividade-N1-4/xadrez.c
@@ -12,14 +12,14 @@ int main() {
     printf("==========================================\n");
 	printf("   A   B   C   D   E   F   G   H\n"); //imprime a linha de cabeC'alho do tabuleiro
 	char* tabuleiro[8][8] = {
-		{"tP1", "cP1", "bP1", "dP", "rP", "pP2", "cP2", "tP2"}, //linha da torre, cavalo, bispo, rei e dama - pretas
+		{"tP1", "cP1", "bP1", "dP", "rP", "bP2", "cP2", "tP2"}, //linha da torre, cavalo, bispo, rei e dama - pretas
 		{"pP1", "pP2", "pP3", "pP4", "pP5", "pP6", "pP7", "pP8"},
 		{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "}, //linha intermediaria - vazias
 		{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
 		{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
 		{"  ", "  ", "  ", "  ", "  ", "  ", "  ", "  "},
 		{"pB1", "pB2", "pB3", "pB4", "pB5", "pB6", "pB7", "pB8"},//linha dos peC5es brancos
-		{"tB1", "cB1", "bB1", "dB", "rB", "dB2", "cB2", "tB2"}// linha da torre, cavalo, bispo, rei e dama - brancas
+		{"tB1", "cB1", "bB1", "dB", "rB", "bB2", "cB2", "tB2"}// linha da torre, cavalo, bispo, rei e dama - brancas
 	};
 	int i = 0; //inicializa o indice para as linhas do tabuleiro
      int j=0; 
@@ -109,7 +109,7 @@ int main() {
     printf("==========================================\n");
     printf("\n-----------Brancas jogam Dh5--------------\n");
 	tabuleiro[7][3] = "...";
-	tabuleiro[3][7] = "dB2";
+	tabuleiro[3][7] = "dB";
 	printf(tabuleiro[i][j]);
     printf("\n");
    i = 0; // redefinindo o indice de linhas
@@ -144,7 +144,7 @@ int main() {
     printf("==========================================\n");
     printf("\n-----------------Brancas capturam Peao em pP6 em f7 e Xeque MATE (Dxf7#)--------------------------\n");
 	tabuleiro[3][7] = "...";
-	tabuleiro[1][5] = "dB2";
+	tabuleiro[1][5] = "dB";
 	printf(tabuleiro[i][j]);
     printf("\n");
    i = 0; // redefinindo o indice de linhas
